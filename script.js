b3375158240678d7c6b7dfc1d5c17b1bff4da2c4
Ancelmo <aj.cardoso@bol.com.br>
2021-01-20 21:59:46 -0300

    criando comida

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG()  {
     context.fillStyle = "lightgreen";
@@ -20,12 +24,17 @@ function criarCobra() {
     }
 }
 
+function drawfood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
 function update (event){
     if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
-    if(event.keyCode == 39 && direction != "left") direction = "reght";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
     if(event.keyCode == 40 && direction != "up") direction = "down"; 
 }
 
@@ -39,6 +48,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobra();
+    drawfood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
